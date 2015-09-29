af2623c441dc92a75985d9e9990f191b94c98aa3
Amir Barylko <amir@barylko.com>
2015-09-28 21:33:54 -0400

    Added tests for expressions bodies on methods and properties

---
+++ CsharpSixDemo/ExpressionBodiesOnMethodsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CsharpSixDemo
+{
+    namespace ExpressionBodiesOnMethods
+    {
+        public class Wizard
+        {
+            public string First { get; } = "Harry";
+
+            public string Last { get; } = "Potter";
+
+            public string FullName() => First + " " + Last;
+
+            public void Print() => Console.WriteLine(First + " " + Last);
+        }
+
+
+        public class FullNameMethod : IAmLazyTest<Wizard>
+        {
+            [Test]
+            public void Combines_first_and_last()
+            {
+                Assert.That(_sut.FullName(), Is.EqualTo("Harry Potter"));
+            }
+        }
+
+    }
+
+}
---
+++ CsharpSixDemo/ExpressionBodiesOnPropertiesTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CsharpSixDemo
+{
+    namespace ExpressionBodiesOnProperties
+    {
+        public class Wizard
+        {
+            public string First { get; } = "Harry";
+
+            public string Last { get; } = "Potter";
+
+            public string FullName => First + " " + Last;
+
+            public string this[string spell] => "Expelliarmus!";
+        }
+
+
+        public class FullNameProperty : IAmLazyTest<Wizard>
+        {
+            [Test]
+            public void Combines_first_and_last()
+            {
+                Assert.That(_sut.FullName, Is.EqualTo("Harry Potter"));
+            }
+        }
+
+        public class SpellIndex : IAmLazyTest<Wizard>
+        {
+            [Test]
+            public void Harry_signature_spell_is_expelliarmus()
+            {
+                Assert.That(_sut["Destroy"], Is.EqualTo("Expelliarmus!"));
+                Assert.That(_sut["Float"]  , Is.EqualTo("Expelliarmus!"));
+            }
+        }
+    }
+
+}
